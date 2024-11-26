3460684c7e83891741c15f7e2b5c32ad28a56499
darrenkjr <darren.rajit@gmail.com>
2024-11-26 11:42:13 +1100

    transferring things to WSL

---
+++ openalex_overarching_search.py
@@ -0,0 +1,6 @@
+
+
+
+topic_id = "placeholder"
+openalex_path = f"https://api.openalex.org/works?filter=primary_topic.id:{topic_id},type:article|review,from_publication_date:1990-01-01,to_publication_date:2022-09-30&per-page=200&cursor=*" 
+
