8e9a3b78a393c6194b83c9c66769d4b4a0e47841
darrenkjr <darren.rajit1@monash.edu>
2024-12-16 10:20:44 +1100

    fixed function import

--- openalex_overarching_search.py
+++ openalex_overarching_search.py
@@ -5,14 +5,14 @@ import aiohttp
 from libraries.openalex_client import OpenAlexClient
 import pyarrow as pa
 import pyarrow.parquet as pq
-from convenience_scripts.openalex_topicsearch_builder import build_topic_ids
+from convenience_scripts.openalex_topicsearch_builder import generate_openalex_topicsearch_id
 from pathlib import Path
 
 
 async def main(): 
     async with OpenAlexClient() as oa_client: 
         #generate topic ids 
-        topic_id_list = build_topic_ids(topic_id_list)
+        topic_id_list = generate_openalex_topicsearch_id(topic_id_list)
 
         #results path 
         current_dir = Path(__file__).parent
