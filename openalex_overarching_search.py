9d22d46527cbe039493b42fe2252881aa8683cf5
darrenkjr <darren.rajit1@monash.edu>
2024-12-16 10:19:11 +1100

    added line to generate topic id list and run overarching OA search

--- openalex_overarching_search.py
+++ openalex_overarching_search.py
@@ -5,20 +5,26 @@ import aiohttp
 from libraries.openalex_client import OpenAlexClient
 import pyarrow as pa
 import pyarrow.parquet as pq
+from convenience_scripts.openalex_topicsearch_builder import build_topic_ids
+from pathlib import Path
 
 
-
-topic_id_list = ['https://openalex.org/T10390', 'https://openalex.org/T10290', 'https://openalex.org/T10351', 'https://openalex.org/T11144', 'https://openalex.org/T10196', 'https://openalex.org/T10263', 'https://openalex.org/T10459', 'https://openalex.org/T10499', 'https://openalex.org/T10560', 'https://openalex.org/T11888', 'https://openalex.org/T13430']
-#test topic id 
 async def main(): 
     async with OpenAlexClient() as oa_client: 
+        #generate topic ids 
+        topic_id_list = build_topic_ids(topic_id_list)
+
+        #results path 
+        current_dir = Path(__file__).parent
+        results_path = current_dir / 'search_results' / 'openalex'
+
         for topic_id in topic_id_list: 
             results_df = await oa_client.get_paginated_results(topic_id)
             result_table = pa.Table.from_pandas(results_df)
         
             topic_name = topic_id.split('/')[-1]
             print('saving results for topic: ', topic_name)
-            pq.write_table(result_table, f'search_results/openalex/topic_searchresults_{topic_name}.parquet')
+            pq.write_table(result_table, results_path / f'topic_searchresults_{topic_name}.parquet')
 
 if __name__ == '__main__': 
     asyncio.run(main())
